afe5b6e01a9060b87bfdabb2448a2c0ebcb51f21
piry1 <piry3943@gmail.com>
2018-02-22 12:32:40 +0100

    add elem model

---
+++ DesktopApi/Data/Model/Elem.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+namespace DesktopApi.Data.Model
+{
+    public class Elem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Path { get; set; } = string.Empty;
+        public string Icon { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public PathType Type { get; set; }
+
+        public enum PathType { File, Directory }
+
+        public bool Exist()
+        {
+            return Type == PathType.Directory
+                ? Directory.Exists(Path)
+                : File.Exists(Path);
+        }
+    }
+}
